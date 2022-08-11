628ec08d3b41dbe00a7dff287ecc18c37052c781
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-11 21:59:03 +0200

    Stupid changes for daily commit

--- EventListener/sandbox.js
+++ EventListener/sandbox.js
@@ -2,7 +2,7 @@ let input = document.querySelector('input');
 
 //This Code should work
 input.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13){
+    if(e.keyCode === 12){
         console.log(e.target.value);
     }
 })

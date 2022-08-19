e5ff118bd800de4bbb48239eba9b49745278f542
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-19 22:46:28 +0200

    Random Commit

--- EventListener/sandbox.js
+++ EventListener/sandbox.js
@@ -5,6 +5,10 @@ input.addEventListener('keyup', (e) => {
     if(e.keyCode === 13){
         console.log(e.target.value);
     }
+
+    if(e.keyCode === 12){
+        console.log(e.target.value);
+    }
 })
 
 array.forEach(element => {

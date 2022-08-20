7067916503ee7c35dd68f82c0634939b7cee7c18
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-08-20 22:46:41 +0200

    removing uneccessory code

--- EventListener/sandbox.js
+++ EventListener/sandbox.js
@@ -5,10 +5,6 @@ input.addEventListener('keyup', (e) => {
     if(e.keyCode === 13){
         console.log(e.target.value);
     }
-
-    if(e.keyCode === 12){
-        console.log(e.target.value);
-    }
 })
 
 array.forEach(element => {
